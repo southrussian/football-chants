0b280cccf0f9c45a9e42a72bb9b7833479f2a8ac
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2023-10-27 16:39:16 +0300

    Initial Commit

---
+++ footballChants/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  footballChants
+//
+//  Created by Danil Peregorodiev on 27.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
