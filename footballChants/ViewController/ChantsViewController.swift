6ed9b70ce389095c931fca177a64897cf98adce2
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2023-11-06 00:07:54 +0300

    Added data source for tableView

---
+++ footballChants/ViewController/ChantsViewController.swift
@@ -0,0 +1,74 @@
+//
+//  ChantsViewController.swift
+//  footballChants
+//
+//  Created by Danil Peregorodiev on 05.11.2023.
+//
+
+import UIKit
+
+class ChantsViewController: UIViewController {
+    
+    // MARK: UI
+
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.backgroundColor = .clear
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 44
+        tableView.separatorStyle = .none
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        
+        return tableView
+    }()
+
+    override func loadView() {
+        super.loadView()
+        setup()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .clear
+    }
+}
+
+private extension ChantsViewController {
+    func setup() {
+        
+        tableView.dataSource = self
+        
+        self.view.addSubview(tableView)
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor)
+
+        ])
+    }
+}
+
+extension ChantsViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        switch indexPath.row {
+        case 0:
+            cell.backgroundColor = .systemTeal
+        case 1:
+            cell.backgroundColor = .systemGray
+        case 2:
+            cell.backgroundColor = .systemPink
+        default:
+            break
+        }
+        return cell
+    }
+}
