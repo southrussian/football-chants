6de0b8b373207f0ce78df44b099cd82734b424e1
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2023-11-07 00:34:44 +0300

    Created Model, added ViewModel

---
+++ footballChants/Model/Manager.swift
@@ -0,0 +1,8 @@
+//
+//  Manager.swift
+//  footballChants
+//
+//  Created by Danil Peregorodiev on 07.11.2023.
+//
+
+import Foundation
---
+++ footballChants/Model/Team.swift
@@ -0,0 +1,8 @@
+//
+//  Team.swift
+//  footballChants
+//
+//  Created by Danil Peregorodiev on 07.11.2023.
+//
+
+import Foundation
