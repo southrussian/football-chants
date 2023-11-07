90dede8005294acfb3caf6955f22882dd4cb31b0
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2023-11-08 00:59:35 +0300

    Added View's vars

--- footballChants/View/TeamTableViewCell.swift
+++ footballChants/View/TeamTableViewCell.swift
@@ -17,4 +17,29 @@ class TeamTableViewCell: UITableViewCell {
         return view
     }()
     
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.spacing = 4
+        stackView.axis = .vertical
+        return stackView
+    }()
+    
+    private lazy var jobLabelView: UILabel = {
+        let labelView = UILabel()
+        labelView.translatesAutoresizingMaskIntoConstraints = false
+        labelView.numberOfLines = 0
+        labelView.font = .systemFont(ofSize: 10, weight: .light)
+        labelView.textColor = .white
+        return labelView
+    }()
+    
+    private lazy var infoLabelView: UILabel = {
+        let labelView = UILabel()
+        labelView.translatesAutoresizingMaskIntoConstraints = false
+        labelView.numberOfLines = 0
+        labelView.font = .systemFont(ofSize: 12, weight: .medium)
+        labelView.textColor = .white
+        return labelView
+    }()
 }
