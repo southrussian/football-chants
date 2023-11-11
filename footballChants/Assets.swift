4ca628b5fa7649639f2133d66b8c50dbc5f5d1c8
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2023-11-11 17:06:22 +0300

    Added Assets

---
+++ footballChants/Assets.swift
@@ -0,0 +1,14 @@
+//
+//  Assets.swift
+//  footballChants
+//
+//  Created by Danil Peregorodiev on 11.11.2023.
+//
+
+import Foundation
+import UIKit
+
+enum Assets {
+    static let play = UIImage(systemName: "play.circle.fill", withConfiguration: UIImage.SymbolConfiguration(pointSize: 32))
+    static let pause = UIImage(systemName: "pause.circle.fill", withConfiguration: UIImage.SymbolConfiguration(pointSize: 32))
+}
